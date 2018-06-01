66d682cfa24b1d259f958b4db801fc3c0e33243b
skyscience <skyscience123@outlook.com>
2018-06-02 00:16:13 +0800

    6/2 0:14

--- test4/res/js/config.js
+++ test4/res/js/config.js
@@ -1,6 +1,4 @@
-// 6/1 11:01 修正
-
-
+//6/2 0:14 修正
 "use strict";
 
 var obj1Info = function (text) {
@@ -32,6 +30,8 @@ var ConstantContract = function() {
 	LocalContractStorage.defineProperty(this, "list");  //列表
 };
 
+
+
 ConstantContract.prototype = {
 	init: function() {
 		this.list = 0;  //初始化序号为0
@@ -57,9 +57,9 @@ ConstantContract.prototype = {
 
 
 		
-		var obj1 = new obj1Info();
-		obj1.value = 0.001;		//无人出价
-		obj1.author = '';	   //无人钱包地址;
+		var obj1 = new obj1Info(); //历史
+		obj1.value = 0.001;		//初始化 出价
+		obj1.author = '';	   //初始化 出价钱包地址;
 		this.infoMap.put(key,JSON.stringify(obj));    //保存物品信息
 		this.moneyMap.put(key,JSON.stringify(obj1)); //保存出价金额
 	},
@@ -81,39 +81,38 @@ ConstantContract.prototype = {
 
 
 	//拍卖竞价
-	bid: function(key1) {  // 卖家钱包地址
+	bid: function(key1) {  // 商品序号
 		var obj1 = new obj1Info(); //历史 出价
 		var obj2 = new obj2Info(); //当前 出价
 		obj2.value = Blockchain.transaction.value;//获取拍卖出价
-		obj2.author = Blockchain.transaction.from; //买家钱包地址;
+		obj2.author = Blockchain.transaction.from; //买家钱包地址
 		
 
+		
 		if(JSON.parse(this.infoMap.get(key1))==="") {
 			Blockchain.transfer('n1JJ8FKGy1kNvna4RTuQyVRcaeV5KLgD8rK',obj2.value); // 将拍卖出价转给我们的账户
 			throw new Error("Error Empty!");  //为空
 		}
-
-
-		// var money = JSON.parse(this.moneyMap.get(key1));//获取最高价格
 		if(obj2.value < obj1.value) {   //拍卖价格 < 最高价格
 			Blockchain.transfer('n1JJ8FKGy1kNvna4RTuQyVRcaeV5KLgD8rK',obj2.value); // 将拍卖出价转给我们的账户
 			throw new Error("ERROR!Your money < Biggest money");//未大于 起拍价！
 		}
-
-
 		var c = obj2.value - obj1.value;
 		if(c < 0.001) {
 			Blockchain.transfer('n1JJ8FKGy1kNvna4RTuQyVRcaeV5KLgD8rK',obj2.value);// 将拍卖出价转给我们的账户
 			throw new Error("ERROR!Your money - Biggest money < 0.001");//价格差过低
 		}
 		
-	
+		
+
+		obj1.value = obj2.value;  //存入历史价格
+		obj1.author = obj2.author; //存入历史买家
 		var amount = new BigNumber(obj2.value);  //获取当前最高价
-		var fk = Blockchain.transfer(obj2.author,amount); //将当前的价 退还给买家
+		var fk = Blockchain.transfer(obj2.author,amount); //将当前的价 退还给当前出价的买家
 		if(!fk) {
 			throw new Error("ERROR FK!"); //退款失败
 		}
-		this.moneyMap.set(key1,obj2); //将最高价格 存入
+		this.moneyMap.put(key1,obj1); //将最高价格 对应到商品key
 	},
 
 
@@ -124,30 +123,34 @@ ConstantContract.prototype = {
 		if(JSON.parse(this.infoMap.get(key2))==="") {
 			throw new Error("Error Empty!");
 		}
-		var dz = JSON.parse(this.infoMap.get(key2).author); //获取卖家地址
+		
+
+		var mjdz = JSON.parse(this.infoMap.get(key2).author); //获取卖家地址
 		var author = Blockchain.transaction.from; //获取买家地址
-		var time = Blockchain.transaction.timestamp; //获取当前时间戳
-		var time1 = JSON.parse(this.infoMap.get(key2).end); //获取终止的时间戳
+		var nowtime = Blockchain.transaction.timestamp; //获取当前时间戳
+		var endtime = JSON.parse(this.infoMap.get(key2).end); //获取终止的时间戳
 		
 
 
-
-		if(dz != author) {  //如果卖家地址不等于买家地址
+		if(mjdz != author) {  //如果卖家地址不等于买家地址
 			throw new Error("Not Local!");//地址匹配失败
 		}	
-		var count = time1 - time;
+		var count = endtime - nowtime;
 		if(count > 0) {
-			throw new Error("not done!");//您没有拍得该此拍卖或此拍卖尚未截止
+			throw new Error("End Time!");//您没有拍得该此拍卖或此拍卖尚未截止
 		}
 
 
-		var bzz = JSON.parse(this.infoMap.get(key2).value); //获取物品保证金
-		var bidmoney = JSON.parse(this.moneyMap.get(key2))*0.99+bzz;// 拍卖金额*(1-手续费比例)＋保证金 
-		var sxfmoney = JSON.parse(this.moneyMap.get(key2))*0.01;  //拍卖金额*手续费
-		Blockchain.transfer(dz,bidmoney); //转给卖家
-		Blockchain.transfer('n1JJ8FKGy1kNvna4RTuQyVRcaeV5KLgD8rK',sxfmoney); //转给 我们
+
+		var money = JSON.parse(this.moneyMap.get(key2).value); //获取拍卖金额
+		var bidmoney = money*0.99+0.2;// 拍卖金额*(1-手续费比例)＋保证金 
+		var sxfmoney = money*0.01;  //拍卖金额*手续费
+		Blockchain.transfer(mjdz,bidmoney); //转给卖家
+		Blockchain.transfer('n1JJ8FKGy1kNvna4RTuQyVRcaeV5KLgD8rK',sxfmoney); //将手续费转给 我们
 	}
 };
+
+
 
 
 module.exports = ConstantContract;
