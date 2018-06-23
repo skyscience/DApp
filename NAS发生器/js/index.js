50632fc9e6df9355fe620a5a963e2bcbdd6eacce
skyscience <skyscience123@outlook.com>
2018-06-23 19:01:57 +0800

    test 0.3

--- NAS发生器/js/index.js
+++ NAS发生器/js/index.js
@@ -1,7 +1,18 @@
 var dappAddress = "n1t7CvSXrB6P9zMvoETjd26B2uUkRGyH5Z4";
+var url_list = [];
+var count = 0;
+var zz_list = [];
+var tempStr = '';
+var lqc = '';
+
 $(function () {
 	var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
 	var nebpay = new NebPay();
+	
+
+
+
+	//赞助列表
 	$("#all").click(function () {
 		var to = dappAddress;
 		var value = "0";
@@ -20,36 +31,47 @@ $(function () {
 					return;
 				}
 
-				var tempStr = '<div class="col-md-3 text-center">';
-
-				for (var i = 0; i < res.length-1; i++) {
+				
+				for (var i = 0; i < res.length; i++) {
+					
+					tempStr += '<div class="col-md-3 text-center">';
 					if (i % 2 == 0) { //0 2 4 6 8
-						tempStr += '<span class="icons c1"><i class="fa fa-trophy"></i></span>';
+						tempStr += '<span class="icons c3"><i class="fa fa-trophy"></i></span>';
 					} else { //1 3 4 5 6
-						tempStr += '<span class="icons c2"><i class="fa fa-picture-o"></i></span>';
+						tempStr += '<span class="icons c4"><i class="fa fa-picture-o"></i></span>';
 					}
+					
 					tempStr += '<div class="box-area"><h3>';
 					tempStr += res[i].title;
 					tempStr += '</h3><p>';
 					tempStr += res[i].info + '<br>';
 					tempStr += '<small><cite>' + 'ID:' + res[i].author.substr(3, 6) + '</cite></small>';
-					tempStr += '</p> </div> ';
-					tempStr += '</div>';
+					tempStr += '</p>';
+					tempStr += '<input id="ck' + i + '" class="ck" onclick="ck(' + i + ')" value="查看" />';
+					url_list.push(res[i].http);   
+					tempStr += '</div></div> ';
 				}
-				console.log(tempStr);
+				
+
 				$("#searchresult").html(tempStr);
 			}
 		});
 	});
-	//列表
-	$("#up").click(function ()  {
+
+
+
+
+
+
+	//快速领取
+	$("#up").click(function () {
 		var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
 		var nebpay = new NebPay();
 		var address = $("#address").val(); //地址
 		address = address.replace(/\n/g, "<br>");
-		nebpay.call(dappAddress, "0", "receive_1", "[\"" +"\"]", {
+		nebpay.call(dappAddress, "0", "receive_1", "[\"" + "\"]", {
 			listener: function Push(resp) {
-				console.log("response of push: " + JSON.stringify(resp))
+				// console.log("response of push: " + JSON.stringify(resp))
 				var respString = JSON.stringify(resp);
 				if (respString.search("rejected by user") !== -1) {
 					alert("关闭交易,取消")
@@ -59,8 +81,11 @@ $(function () {
 			}
 		})
 	});
-	
-	$("#ups").click(function ()  {
+
+
+
+
+	$("#ups").click(function () {
 		var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
 		var nebpay = new NebPay();
 		var address = $("#address").val(); //地址
@@ -75,7 +100,7 @@ $(function () {
 		address = address.replace(/\n/g, "<br>");
 		nebpay.call(dappAddress, "0", "receive", "[\"" + address + "\"]", {
 			listener: function Push(resp) {
-				console.log("response of push: " + JSON.stringify(resp))
+				// console.log("response of push: " + JSON.stringify(resp))
 				var respString = JSON.stringify(resp);
 				if (respString.search("rejected by user") !== -1) {
 					alert("关闭交易,取消")
@@ -86,16 +111,19 @@ $(function () {
 		})
 	});
 
+
+
+	//发布赞助
 	$("#saven").click(function () {
 		var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
 		var nebpay = new NebPay();
-	
+
 		var name = $("#name").val();    //名称
 		var content = $("#conte").val();	//内容
 		var img = $("#img").val();		//图片
 		var url = $("#url").val();		//地址
 		var bal = $("#bal").val();
-	
+
 		if (name == "") {
 			alert("请输入物品名称。");
 			return;
@@ -108,19 +136,19 @@ $(function () {
 			alert("赞助费过低");
 			return;
 		}
-	
+
 		name = name.replace(/\n/g, "<br>");
 		content = content.replace(/\n/g, "<br>");
 		url = url.replace(/\n/g, "<br>");
 		img = img.replace(/\n/g, "<br>");
-	
-	
+
+
 		var to = dappAddress;
 		var callFunction = "savenew";
 		var callArgs = '["' + name + '","' + content + '","' + img + '","' + url + '"]';
 		nebpay.call(to, bal, callFunction, callArgs, {
 			listener: function Push(resp) {
-				console.log("response of push: " + JSON.stringify(resp))
+				// console.log("response of push: " + JSON.stringify(resp))
 				var respString = JSON.stringify(resp);
 				if (respString.search("rejected by user") !== -1) {
 					alert("关闭交易,取消上传")
@@ -130,12 +158,62 @@ $(function () {
 			}
 		});
 	});
-	$("#all").click();
 });
 
 
+//=====================我是一条华丽的分割线===================
 
 
+
+//查看赞助 领取nas
+function ck( i ){
+	
+	window.open(url_list[i]);
+
+	if (count == 9){
+		alert('抱歉，您已达到今天的领取上限');
+		return;
+	}
+	for(var j = 0; j < zz_list.length; j++){
+		if(zz_list[j] == i){
+			alert('抱歉，您已经领取过这个赞助商的nas了，试试其它的吧~');
+			return;
+		}
+	}
+
+	count += 1;  //增加领取次数
+	var cs = document.getElementById('lqcs');
+	cs.value='已领取 '+ count +' / 10 次';
+	
+	zz_list.push(i);  //将领取过的赞助序号 放进数组方便判重
+
+
+	var Obtn = document.getElementById('ck'+i+'');
+	Obtn.value='已领取';
+	Obtn.className = 'ck1';
+	
+
+	var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
+	var nebpay = new NebPay();
+	var address = $("#address").val(); //地址
+	address = address.replace(/\n/g, "<br>");
+	nebpay.call(dappAddress, "0", "receive_1", "[\"" + "\"]", {
+		listener: function Push(resp) {
+			// console.log("response of push: " + JSON.stringify(resp))
+			var respString = JSON.stringify(resp);
+			if (respString.search("rejected by user") !== -1) {
+				alert("关闭交易,取消")
+			} else if (respString.search("txhash") !== -1) {
+				alert("上传Hash: " + resp.txhash + "请等待交易确认")
+			}
+		}
+	})
+	console.log('领取次数:  '+count);
+	console.log('领取赞助列表:  '+zz_list);
+}
+
+
+//转换时间戳
 function ttt(timestamp) {
 	var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
 	Y = date.getFullYear() + '-';
